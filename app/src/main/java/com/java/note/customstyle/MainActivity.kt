6ea028358e28ec449cdf9879f4b9c3ca189f920a
Yulia <123>
2019-01-28 23:38:12 +0400

    all project

--- app/src/main/java/com/java/note/customstyle/MainActivity.kt
+++ app/src/main/java/com/java/note/customstyle/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.java.note.customstyle
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (AppCompatDelegate.getDefaultNightMode()
+            == AppCompatDelegate.MODE_NIGHT_YES
+        ) {
+            setTheme(R.style.DarkTheme)
+        } else {
+            setTheme(R.style.AppTheme)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (AppCompatDelegate.getDefaultNightMode()
+            == AppCompatDelegate.MODE_NIGHT_YES
+        ) {
+            switchId.isChecked = true
+        }
+        switchId.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                restartApp()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                restartApp()
+            }
+        }
+    }
+
+    private fun restartApp() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
